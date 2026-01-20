914f869b1735baf0c052ad9fc2ea52b4937ea0a4
Tejas01-developes <tlimbad7@gmail.com>
2026-01-21 01:56:54 +0530

    update

--- acceptreject/src/App.jsx
+++ acceptreject/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {Route, Routes} from 'react-router-dom';
 import First from './pages/First';
+import Req from './pages/Req';
 const App = () => {
   return (
     <div>
@@ -8,6 +9,8 @@ const App = () => {
     {/* 1.code for first page */}
 <Route path='/' element={<First/>}/>
 
+{/* 2.code for req page */}
+<Route path='/req' element={<Req/>}/>
 
 
     </Routes>
---
+++ acceptreject/src/pages/Req.jsx
@@ -0,0 +1,61 @@
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+
+const Req = () => {
+const[req,setreq]=useState([])
+useEffect(()=>{
+const getreq=async()=>{
+    const getrequrl=await axios.get("http://localhost:3000/apis/get");
+    if(getrequrl.data.success){
+return setreq(getrequrl.data.data)
+    }
+    alert(getrequrl.data.message);
+}
+getreq()
+},[])
+
+
+const accept=async(name)=>{
+    const payload={
+        email:name
+        
+    }
+console.log(payload)
+    const accurl=await axios.post("http://localhost:3000/apis/accept",payload)
+    if(accurl.data.success){
+        alert(accurl.data.message)
+      
+       return
+    }
+    alert("error")
+}
+
+
+const reject=async(name)=>{
+    const payload={
+        email:name
+    }
+    const rejurl=await axios.post("http://localhost:3000/apis/reject",payload)
+    if(rejurl.data.success){
+       return alert(rejurl.data.message)
+    }
+    alert("error")
+}
+
+  return (
+    <div>
+  {
+req.map((r,i)=>(
+    <div key={i}>
+        {r.name}:     
+        <strong>{r.status}</strong>
+        <button><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRI6XgMKd2_iMityOS59F8HHoQLizCwzz4NQA&s" alt="" onClick={()=>accept(r.name)} /></button>
+        <button><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTH3oqFQYeq2Jniv1Tzq38wF48wrhyDNdMKbA&s" alt="" onClick={()=>reject(r.name)} /></button>
+    </div>
+))
+  }
+    </div>
+  )
+}
+
+export default Req
